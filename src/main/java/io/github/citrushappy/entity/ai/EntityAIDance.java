44a5e1d51a187ac776ea4037d904da0e83da4d77
Citrus <36286623+CitrusHappy@users.noreply.github.com>
2023-03-12 14:24:03 -0700

    crewmate entity class cleanup

--- src/main/java/io/github/citrushappy/entity/ai/EntityAIDance.java
+++ src/main/java/io/github/citrushappy/entity/ai/EntityAIDance.java
@@ -3,10 +3,10 @@ package io.github.citrushappy.entity.ai;
 import io.github.citrushappy.entity.passive.EntityCrewMate;
 import net.minecraft.entity.ai.EntityAIBase;
 
-public class EntityAIIdle extends EntityAIBase {
+public class EntityAIDance extends EntityAIBase {
     protected final EntityCrewMate entity;
 
-    public EntityAIIdle(EntityCrewMate creatureIn){
+    public EntityAIDance(EntityCrewMate creatureIn){
         this.entity = creatureIn;
     }
 
@@ -18,13 +18,7 @@ public class EntityAIIdle extends EntityAIBase {
         return false;
     }
 
-    public boolean shouldContinueExecuting()
-    {
-        return this.entity.isPartying();
-    }
-
-    @Override
-    public boolean isInterruptible() {
-        return false;
+    public void startExecuting() {
+        this.entity.getNavigator().clearPath();
     }
 }
--- src/main/java/io/github/citrushappy/entity/passive/EntityCrewMate.java
+++ src/main/java/io/github/citrushappy/entity/passive/EntityCrewMate.java
@@ -1,10 +1,12 @@
 package io.github.citrushappy.entity.passive;
 
-import io.github.citrushappy.entity.ai.EntityAIIdle;
+import io.github.citrushappy.entity.ai.EntityAIDance;
+import io.github.citrushappy.entity.monster.EntityImposter;
 import io.github.citrushappy.util.Reference;
 import io.github.citrushappy.init.SoundsHandler;
 import net.minecraft.block.Block;
 import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.ai.*;
 import net.minecraft.entity.player.EntityPlayer;
@@ -38,46 +40,21 @@ public class EntityCrewMate extends EntityCreature implements IAnimatable, IAnim
     public static final ResourceLocation LOOT = new ResourceLocation(Reference.MOD_ID, "entities/crewmate");
 
     private static final DataParameter<Integer> CREWMATE_VARIANT = EntityDataManager.createKey(EntityCrewMate.class, DataSerializers.VARINT);
-    int timeToSpeak = 200;
-    boolean canPlayFallSound = false;
+    private int timeToSpeak = 200;
+    private boolean canPlayFallSound = false;
     private boolean isPartying = false;
     private BlockPos jukeboxPosition;
 
-
     private final AnimationFactory factory = new AnimationFactory(this);
 
-    private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
-        event.getController().setAnimationSpeed(1D);
-        //TODO: MAKE THIS A SWITCH CASE PLS
-        if(this.isPartying){
-            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.crewmate.dance", true));
-            event.getController().setAnimationSpeed(3D);
-            return PlayState.CONTINUE;
-        }
-        if (event.isMoving())
-        {
-            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.crewmate.walk", true));
-            return PlayState.CONTINUE;
-        }
-        if(isDead())
-        {
-            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.crewmate.death.pose", true));
-            return PlayState.CONTINUE;
-        }
-        event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.crewmate.idle", true));
-        return PlayState.CONTINUE;
-    }
-
-    public EntityCrewMate(World worldIn)
-    {
+    public EntityCrewMate(World worldIn) {
         super(worldIn);
         this.ignoreFrustumCheck = true;
         this.setSize(0.7F, 1.3F);
     }
 
     @Nullable
-    public IEntityLivingData onInitialSpawn(DifficultyInstance difficulty, @Nullable IEntityLivingData livingdata)
-    {
+    public IEntityLivingData onInitialSpawn(DifficultyInstance difficulty, @Nullable IEntityLivingData livingdata) {
         livingdata = super.onInitialSpawn(difficulty, livingdata);
         int i = this.getRandomCrewMateType();
 
@@ -95,65 +72,51 @@ public class EntityCrewMate extends EntityCreature implements IAnimatable, IAnim
         return livingdata;
     }
 
-    @Nullable
-    @Override
-    protected ResourceLocation getLootTable() {
-        return LOOT;
+
+    protected void entityInit() {
+        super.entityInit();
+        this.dataManager.register(CREWMATE_VARIANT, 0);
     }
 
     @Override
-    public void registerControllers(AnimationData data)
+    protected void initEntityAI()
     {
-        data.addAnimationController(new AnimationController(this, "controller", 1, this::predicate));
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(1, new EntityAIAvoidEntity(this, EntityImposter.class, 8.0F, 0.6D, 0.6D));
+        this.tasks.addTask(1, new EntityAIDance(this));
+        this.tasks.addTask(2, new EntityAIMoveIndoors(this));
+        this.tasks.addTask(3, new EntityAIRestrictOpenDoor(this));
+        this.tasks.addTask(4, new EntityAIOpenDoor(this, true));
+        this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 0.6D));
+        this.tasks.addTask(9, new EntityAIWatchClosest2(this, EntityPlayer.class, 3.0F, 1.0F));
+        this.tasks.addTask(9, new EntityAIWanderAvoidWater(this, 0.6D));
+        this.tasks.addTask(10, new EntityAIWatchClosest(this, EntityLiving.class, 8.0F));
     }
 
     @Override
-    public AnimationFactory getFactory()
-    {
-        return this.factory;
-    }
-
-    @Override
-    public void onUpdate()
-    {
+    public void onUpdate() {
         super.onUpdate();
-        if(timeToSpeak > 0)
-        {
+        if(timeToSpeak > 0) {
             timeToSpeak--;
         }
     }
 
     public void onLivingUpdate()
     {
-        if (this.jukeboxPosition == null || this.jukeboxPosition.distanceSq(this.posX, this.posY, this.posZ) > 34.0D || this.world.getBlockState(this.jukeboxPosition).getBlock() != Blocks.JUKEBOX)
-        {
+        if (this.jukeboxPosition == null || this.jukeboxPosition.distanceSq(this.posX, this.posY, this.posZ) > 34.0D || this.world.getBlockState(this.jukeboxPosition).getBlock() != Blocks.JUKEBOX) {
             this.isPartying = false;
             this.jukeboxPosition = null;
         }
 
-        if(!this.onGround && canPlayFallSound && this.motionY < -0.8D)
-        {
+        if(!this.onGround && canPlayFallSound && this.motionY < -0.8D) {
             canPlayFallSound = false;
             this.playSound(SoundsHandler.ENTITY_CREWMATE_FALL_BIG, 1f, 1f);
         }
-        if(this.onGround)
-        {
+        if(this.onGround) {
             canPlayFallSound = true;
         }
 
         super.onLivingUpdate();
-    }
-
-    @Override
-    protected void initEntityAI()
-    {
-        this.tasks.taskEntries.clear();
-        this.targetTasks.taskEntries.clear();
-        this.tasks.addTask(0, new EntityAIIdle(this));
-        this.tasks.addTask(1, new EntityAIMoveTowardsRestriction(this, 1.0D));
-        this.tasks.addTask(2, new EntityAIWander(this, 0.4D));
-        this.tasks.addTask(3, new EntityAIWatchClosest(this, EntityPlayer.class, 5.0F));
-        this.tasks.addTask(4, new EntityAILookIdle(this));
     }
 
     @Override
@@ -166,65 +129,88 @@ public class EntityCrewMate extends EntityCreature implements IAnimatable, IAnim
         super.onUpdate();
     }
 
-    //play sounds
     @Override
-    protected void playStepSound(BlockPos pos, Block blockIn) {
-        this.playSound(this.getStepSound(), 1F, 1F);
-    }
-
-
-    //get sounds
-    @Override
-    protected SoundEvent getAmbientSound()
-    {
-        return SoundsHandler.ENTITY_CREWMATE_AMBIENT;
-    }
-
-    @Override
-    protected SoundEvent getHurtSound(DamageSource source)
-    {
-        return SoundsHandler.ENTITY_CREWMATE_HURT;
-    }
-
-    @Override
-    protected SoundEvent getDeathSound()
-    {
-
-        return SoundsHandler.ENTITY_CREWMATE_DEATH;
-    }
-
-    protected SoundEvent getStepSound()
-    {
-
-        return SoundsHandler.ENTITY_CREWMATE_STEP_GENERIC;
-    }
-
-
-    @Override
-    public void onCollideWithPlayer(EntityPlayer entityIn)
-    {
-        if(!isPartying() && timeToSpeak <= 0 && this.isEntityAlive() && this.rand.nextInt(1000) > this.livingSoundTime++)
-        {
+    public void onCollideWithPlayer(EntityPlayer entityIn) {
+        if(!isPartying() && timeToSpeak <= 0 && this.isEntityAlive() && this.rand.nextInt(1000) > this.livingSoundTime++) {
             this.playSound(SoundsHandler.ENTITY_CREWMATE_PLAYER_COLLISION, getSoundVolume(), 1f);
             timeToSpeak = 200;
         }
     }
 
 
+    //Animation Control
+    //==================
+    private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
+        String animationName;
+        double animationSpeed = 1D;
+        AnimationBuilder animationBuilder = new AnimationBuilder();
+
+        if(this.isPartying) {
+            animationName = "animation.crewmate.dance";
+            animationSpeed = 3D;
+        }
+        else if(event.isMoving()) {
+            //TODO: this gets called way too late after dancing
+            animationName = "animation.crewmate.walk";
+        }
+        else if(isDead()) {
+            animationName = "animation.crewmate.death.pose";
+        }
+        else {
+            animationName = "animation.crewmate.idle";
+        }
+
+        animationBuilder.addAnimation(animationName, true);
+        event.getController().setAnimation(animationBuilder);
+        event.getController().setAnimationSpeed(animationSpeed);
+
+        return PlayState.CONTINUE;
+    }
 
     @Override
-    protected float getSoundVolume()
-    {
-        return 0.4F;
+    public void registerControllers(AnimationData data) {
+        data.addAnimationController(new AnimationController(this, "controller", 1, this::predicate));
+    }
+
+    @Override
+    public AnimationFactory getFactory() {
+        return this.factory;
     }
 
 
-    protected void entityInit()
-    {
-        super.entityInit();
-        this.dataManager.register(CREWMATE_VARIANT, Integer.valueOf(0));
+    //Sound Control
+    //==================
+    @Override
+    protected void playStepSound(BlockPos pos, Block blockIn) {
+        this.playSound(this.getStepSound(), 1F, 1F);
     }
 
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return SoundsHandler.ENTITY_CREWMATE_AMBIENT;
+    }
+
+    @Override
+    protected SoundEvent getHurtSound(DamageSource source) {
+        return SoundsHandler.ENTITY_CREWMATE_HURT;
+    }
+
+    @Override
+    protected SoundEvent getDeathSound() {
+        return SoundsHandler.ENTITY_CREWMATE_DEATH;
+    }
+
+    @Override
+    protected float getSoundVolume() { return 0.4F; }
+
+    protected SoundEvent getStepSound() {
+
+        return SoundsHandler.ENTITY_CREWMATE_STEP_GENERIC;
+    }
+
+
+    //Death Logic
+    //==================
     public boolean isDead() {
         return ((this.dead || this.getHealth() < 0.01));
     }
@@ -235,8 +221,7 @@ public class EntityCrewMate extends EntityCreature implements IAnimatable, IAnim
         if (this.deathTime == 80) {
             this.setDead();
 
-            for (int k = 0; k < 20; ++k)
-            {
+            for (int k = 0; k < 20; ++k) {
                 double d2 = this.rand.nextGaussian() * 0.02D;
                 double d0 = this.rand.nextGaussian() * 0.02D;
                 double d1 = this.rand.nextGaussian() * 0.02D;
@@ -246,59 +231,60 @@ public class EntityCrewMate extends EntityCreature implements IAnimatable, IAnim
     }
 
 
+    //Loot Table control
+    //==================
+    @Nullable
+    @Override
+    protected ResourceLocation getLootTable() {
+        return LOOT;
+    }
 
-    //color variant control
-    private int getRandomCrewMateType()
-    {
+
+    //Color variant control
+    //==================
+    private int getRandomCrewMateType() {
         int i = this.rand.nextInt(12);
         return i;
     }
 
-    public int getCrewMateType()
-    {
-        return this.dataManager.get(CREWMATE_VARIANT).intValue();
+    public int getCrewMateType() {
+        return this.dataManager.get(CREWMATE_VARIANT);
     }
 
-    public void setCrewMateType(int crewmateTypeId)
-    {
-        this.dataManager.set(CREWMATE_VARIANT, Integer.valueOf(crewmateTypeId));
+    public void setCrewMateType(int crewmateTypeId) {
+        this.dataManager.set(CREWMATE_VARIANT, crewmateTypeId);
     }
 
-
-    public void writeEntityToNBT(NBTTagCompound compound)
-    {
+    public void writeEntityToNBT(NBTTagCompound compound) {
         super.writeEntityToNBT(compound);
         compound.setInteger("CrewMateType", this.getCrewMateType());
     }
 
-    public void readEntityFromNBT(NBTTagCompound compound)
-    {
+    public void readEntityFromNBT(NBTTagCompound compound) {
         super.readEntityFromNBT(compound);
         this.setCrewMateType(compound.getInteger("CrewMateType"));
     }
 
-    public static class CrewMateTypeData implements IEntityLivingData
-    {
+    public static class CrewMateTypeData implements IEntityLivingData {
         public int typeData;
 
-        public CrewMateTypeData(int type)
-        {
+        public CrewMateTypeData(int type) {
             this.typeData = type;
         }
     }
 
+
+    //Partying control
+    //==================
     @SideOnly(Side.CLIENT)
-    public void setPartying(BlockPos pos, boolean isPartying)
-    {
+    public void setPartying(BlockPos pos, boolean isPartying) {
         this.setRotation(this.rotationYaw + 180f, this.rotationPitch);
         this.jukeboxPosition = pos;
         this.isPartying = isPartying;
     }
 
     @SideOnly(Side.CLIENT)
-    public boolean isPartying()
-    {
+    public boolean isPartying() {
         return this.isPartying;
     }
-
 }
